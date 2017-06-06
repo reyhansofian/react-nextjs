f1b4eea8a00c4bba248f4ecc4ef6f827733ec195
Reyhan Sofian <computecoholic@gmail.com>
2017-06-06 13:46:38 +0700

    fix register syntax (#12)

--- components/Register/Register.js
+++ components/Register/Register.js
@@ -3,61 +3,59 @@ import React, { Component } from 'react';
 class Register extends Component {
 
   constructor(props) {
-      super(props);
+    super(props);
 
-      this.state = {
-         reg_usern: '',
-         reg_email: '',
-         reg_paswd: '',
-         reg_paswd_rep: '',
-         err: false,
-         validated: '',
-      }
+    this.state = {
+      reg_usern: '',
+      reg_email: '',
+      reg_paswd: '',
+      reg_paswd_rep: '',
+      err: false,
+      validated: '',
+    };
 
-      this.onSubmit = this.onSubmit.bind(this);
-      this.onChange = this.onChange.bind(this);
-   };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
 
-   onChange(e){
-      this.setState({ [e.target.name]: e.target.value });
-      console.log('value is > ' + this.state.reg_paswd);
-      console.log('value is > ' + this.state.reg_paswd_rep);
-   }
-   onSubmit(){
-      if(this.state.reg_paswd != this.state.reg_paswd_rep){
-        alert('password did not macth!');
-      }
-      else{
-        fetch('/users', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            username: this.state.username,
-            email: this.state.req_email
-            password: this.state.password,
-          })
-        })
-       .then(function(response){
-          if (response.status >= 400) {
-            throw new Error("Bad response from server");
-          }
-          return response.json();
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(`value is > ${this.state.reg_paswd}`);
+    console.log(`value is > ${this.state.reg_paswd_rep}`);
+  }
+  onSubmit() {
+    if (this.state.reg_paswd != this.state.reg_paswd_rep) {
+      alert('password did not match!');
+    } else {
+      fetch('/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: this.state.username,
+          email: this.state.req_email,
+          password: this.state.password,
+        }),
+      })
+       .then((response) => {
+         if (response.status >= 400) {
+           throw new Error('Bad response from server');
+         }
+         return response.json();
        })
-       .then(function(json){
-          if(json.status == 'success'){
-            alert('success boss');
-          }
-          else{
-            alert('fail boss');
-          }
-       })
-      }
-   }
+       .then((json) => {
+         if (json.status == 'success') {
+           alert('success boss');
+         } else {
+           alert('fail boss');
+         }
+       });
+    }
+  }
 
   render() {
-    const { reg_usern, reg_email, reg_paswd, reg_paswd_rep,validated } = this.state;
+    const { reg_usern, reg_email, reg_paswd, reg_paswd_rep, validated } = this.state;
     return (
       <div className="app flex-row align-items-center">
         <div className="container">
@@ -68,20 +66,20 @@ class Register extends Component {
                   <h1>Register</h1>
                   <p className="text-muted">Create your account</p>
                   <div className="input-group mb-3">
-                    <span className="input-group-addon"><i className="icon-user"></i></span>
-                    <input type="text" className="form-control" name="reg_usern" value={reg_usern} onChange={this.onChange} placeholder="Username"/>
+                    <span className="input-group-addon"><i className="icon-user" /></span>
+                    <input type="text" className="form-control" name="reg_usern" value={reg_usern} onChange={this.onChange} placeholder="Username" />
                   </div>
                   <div className="input-group mb-3">
                     <span className="input-group-addon">@</span>
-                    <input type="text" className="form-control" name="reg_email" value={reg_email} onChange={this.onChange} placeholder="Email"/>
+                    <input type="text" className="form-control" name="reg_email" value={reg_email} onChange={this.onChange} placeholder="Email" />
                   </div>
                   <div className="input-group mb-3">
-                    <span className="input-group-addon"><i className="icon-lock"></i></span>
-                    <input type="password" className="form-control" name="reg_paswd" value={reg_paswd} onChange={this.onChange} placeholder="Password"/>
+                    <span className="input-group-addon"><i className="icon-lock" /></span>
+                    <input type="password" className="form-control" name="reg_paswd" value={reg_paswd} onChange={this.onChange} placeholder="Password" />
                   </div>
                   <div className="input-group mb-4">
-                    <span className="input-group-addon"><i className="icon-lock"></i></span>
-                    <input type="password" className="form-control" name="reg_paswd_rep" value={reg_paswd_rep} onChange={this.onChange} placeholder="Repeat password"/>
+                    <span className="input-group-addon"><i className="icon-lock" /></span>
+                    <input type="password" className="form-control" name="reg_paswd_rep" value={reg_paswd_rep} onChange={this.onChange} placeholder="Repeat password" />
                   </div>
                   <button type="button" className="btn btn-block btn-success" onClick={this.onSubmit}>Create Account</button>
                 </div>
