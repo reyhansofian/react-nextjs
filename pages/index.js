08f311bbee77087878fffef4c01ba1e11a3dcbed
Reyhan Sofian <computecoholic@gmail.com>
2017-06-06 13:28:36 +0700

    server side route (#8)

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
+import Head from 'next/head';
+
 import Header from '../components/Header/Header';
 import Sidebar from '../components/Sidebar/Sidebar';
 import Footer from '../components/Footer/Footer';
 import Login from '../components/Login/Login';
 
-import Head from 'next/head';
-
 class Index extends Component {
-    render() {
-        return (
-            <div>
-                <Head>
-                    <link rel="stylesheet" href="../static/css/simple-line-icons.css" />
-                    <link rel="stylesheet" href="../static/css/style.css" />
-                </Head>
-                <div className="app">
-                    <Login />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Head>
+          <link rel="stylesheet" href="../static/css/simple-line-icons.css" />
+          <link rel="stylesheet" href="../static/css/style.css" />
+        </Head>
+        <div className="app">
+          <Header />
+          <div className="app-body">
+            <Sidebar {...this.props} />
+          </div>
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Index;
