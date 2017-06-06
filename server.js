502112d2d4151c48721bff064648c67a7c21a380
Reyhan Sofian <computecoholic@gmail.com>
2017-06-06 11:21:53 +0700

    add server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare()
+    .then(() => {
+      const server = express();
+
+      server.get('/a', (req, res) => app.render(req, res, '/b', req.query));
+
+      server.get('/b', (req, res) => app.render(req, res, '/a', req.query));
+
+      server.get('*', (req, res) => handle(req, res));
+
+      server.listen(3000, (err) => {
+        if (err) throw err;
+        console.log('> Ready on http://localhost:3000');
+      });
+    });
